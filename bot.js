f9fd4bb0afeccd10fb09ca6226aad2af310162ab
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2018-12-08 21:00:28 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "M";
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+       if (message.content ==='هلا') { 
+            message.reply('** اهلين وسهلين :rose: :heart_eyes: **');
+          }
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+if (command == "embed") {
+    let say = new Discord.RichEmbed()
+  .setThumbnail(message.author.avatarURL)
+  .setAuthor(message.author.username)
+    .setDescription(args.join("  "))
+    .setColor(0x06DF00)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+  });
+
+client.login(process.env.BOT_TOKEN);
